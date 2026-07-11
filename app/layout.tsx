16d166de8e240babb0e033eebc439149e62444b0
Shubham Yadav <shubhamya.1157@gmail.com>
2026-07-11 17:10:40 +0530

    adding all componant of shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { JetBrains_Mono, Oxanium } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const oxaniumHeading = Oxanium({subsets:['latin'],variable:'--font-heading'});
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 export const metadata: Metadata = {
   title: "MG7",
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="en" className={cn("font-mono", jetbrainsMono.variable, oxaniumHeading.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
