4d01a6a330fb6663778c1c0a764fd864e2a1ba9b
Shubham Yadav <shubhamya.1157@gmail.com>
2026-07-11 23:26:12 +0530

    fixing error of infinite call

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { JetBrains_Mono, Oxanium } from "next/font/google";
+import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 
-const oxaniumHeading = Oxanium({subsets:['latin'],variable:'--font-heading'});
+const oxanium = Oxanium({ subsets: ["latin"], variable: "--font-heading" });
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], variable: "--font-mono" });
 
 export const metadata: Metadata = {
   title: "MG7",
-  description: "Build by:shubham yadav",
+  description: "Built by Shubham Yadav",
 };
 
 export default function RootLayout({
@@ -21,13 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en" className={cn("font-mono", jetbrainsMono.variable, oxaniumHeading.variable)}
+      lang="en"
+      className={cn(jetbrainsMono.variable, oxanium.variable)}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col"> 
-
+      <body className="flex min-h-screen flex-col">
         <QueryProvider>
-        
-           <ThemeProvider
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
@@ -35,9 +35,8 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-          </QueryProvider>
-          
-          </body>
+        </QueryProvider>
+      </body>
     </html>
   );
 }
